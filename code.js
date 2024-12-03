4a8a6939ca44e8a9a741f39563424b28538f5cb7
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-12-03 11:22:13 -0700

    Update code.js

--- code.js
+++ code.js
@@ -26,14 +26,14 @@ function searchforCycle(graph, node, visited){
     // marks current node as visisted now
     visited[node] = true;
     // checks each node connected to the current node
-    for(let adjNode of graph[node]) {
+    for(const adjNode of graph[node]) {
         // if the node has been already visited, there is a cycle 
         if(visited[adjNode] === true){
             return true;
         }
         else{
             // searches for cycles related to the current node by checking their connected nodes against the current iteration of visited
-            if(searchforCycle(graph, adjNode,visited)){
+            if(searchforCycle(graph, adjNode, visited)){
                 return true;
             }
         }
